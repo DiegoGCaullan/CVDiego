c1f9b54448180c00ff4741c3af16d37d5becfbf1
Diego Caullan <diegoo_2058@hotmail.com>
2023-02-01 20:17:40 -0300

    Primera escritura en el repositorio

---
+++ js/script.js
@@ -0,0 +1,7 @@
+document.getElementById('boton').onclick = function () {
+console.log("Capturamos el evento click");
+document.getElementById("demo").innerHTML = "Edad: 35 años, Estado Civil: Soltero, CUIT: 20-30235667-8";
+
+
+}
+window.alert("Esto es un Curriculum Vitae");
